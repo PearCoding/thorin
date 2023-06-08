a9d2f776cc2cae1c6c7b560b813cea3850f05412
Ömercan Yazici <yazici@cg.uni-saarland.de>
2023-06-08 20:02:28 +0200

    Update to nvvm 2.0

--- src/thorin/be/llvm/nvvm.cpp
+++ src/thorin/be/llvm/nvvm.cpp
@@ -25,20 +25,20 @@ NVVMCodeGen::NVVMCodeGen(World& world, const Cont2Config& kernel_config, bool de
 {
     auto triple = llvm::Triple(llvm::sys::getDefaultTargetTriple());
     if (triple.isArch32Bit()) {
-        module().setDataLayout("e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64");
+        module().setDataLayout("e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-i128:128:128-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64");
         module().setTargetTriple("nvptx-nvidia-cuda");
     } else {
-        module().setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64");
+        module().setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-i128:128:128-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64");
         module().setTargetTriple("nvptx64-nvidia-cuda");
     }
     // nvvmir.version
     auto nvvmir_version_md = module().getOrInsertNamedMetadata("nvvmir.version");
     llvm::IRBuilder<> irbuilder(context());
-    llvm::Metadata* annotation_values_15[] = {
-        llvm::ConstantAsMetadata::get(irbuilder.getInt64(1)),
-        llvm::ConstantAsMetadata::get(irbuilder.getInt64(5))
+    llvm::Metadata* annotation_values[] = {
+        llvm::ConstantAsMetadata::get(irbuilder.getInt32(2)),
+        llvm::ConstantAsMetadata::get(irbuilder.getInt32(0))
     };
-    nvvmir_version_md->addOperand(llvm::MDNode::get(context(), annotation_values_15));
+    nvvmir_version_md->addOperand(llvm::MDNode::get(context(), annotation_values));
 }
 
 //------------------------------------------------------------------------------
